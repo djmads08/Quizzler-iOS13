c214dbcdf5403086ff0277236bc4c96b894c9b91
djmads08 <47112428+djmads08@users.noreply.github.com>
2021-03-15 06:49:36 +0000

    15 March 2020 - Push

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [["Question 1", "True"], ["Question 2", "True"], ["Question 3", "True"]]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // updateUI displays the first question.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        // When answerButtonPressed is engaged, the questionNumber (used to pull a specific string from the [quiz] array is incremented.
+        // updateUI function is called to display the new question label for the user.
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        // Function changes the Question to the new questionNumber in the array of [quiz].
+        questionLabel.text = quiz[questionNumber]
+    }
+    
+    
 }
 
